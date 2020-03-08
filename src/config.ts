9fd8274144a05ca711ad4ad44872932392454b84
jonathonflorek <jonathonflorek@gmail.com>
2020-03-07 21:53:15 -0600

    implement darksky integration

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const port = required('PORT');
 export const petShelterApiUrl = required('PETSHELTERAPI_URL');
+export const darkSkyUrl = required('DARKSKY_URL');
 
 function required(name: string) {
     const result = process.env[name];
--- src/integration/isItRaining.ts
+++ src/integration/isItRaining.ts
@@ -1,8 +1,13 @@
+import axios from 'axios';
+import { darkSkyUrl } from '../config';
+
 export interface LatLng {
     lat: number;
     lng: number;
 }
 
 export async function isItRaining({ lat, lng }: LatLng): Promise<boolean> {
-    return Math.random() > 0.5 ? true : false;
+    // usage documented in https://darksky.net/dev/docs#forecast-request
+    const response = await axios.get(`${darkSkyUrl}/${lat},${lng}`);
+    return response.data.currently.precipProbability || 0 > 0.5;
 }
