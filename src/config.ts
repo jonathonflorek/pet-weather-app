c87abc68b3dfb15764624cf4116c772bf474fe79
jonathonflorek <jonathonflorek@gmail.com>
2020-03-07 22:33:30 -0600

    implement geocoding

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 export const port = required('PORT');
 export const petShelterApiUrl = required('PETSHELTERAPI_URL');
 export const darkSkyUrl = required('DARKSKY_URL');
+export const googleGeocodingKey = required('GOOGLE_GEOCODING_KEY');
 
 function required(name: string) {
     const result = process.env[name];
--- src/integration/geocoding.ts
+++ src/integration/geocoding.ts
@@ -1,18 +1,38 @@
+import { Client } from '@googlemaps/google-maps-services-js';
+import { googleGeocodingKey } from '../config';
+const client = new Client({});
+
 export interface LatLng {
     lat: number;
     lng: number;
 }
 
 export async function geocode(address: string): Promise<LatLng | undefined> {
-    if (Math.random() > 0.5) {
-        return undefined;
+    const response = await client.geocode({
+        params: {
+            address,
+            key: googleGeocodingKey,
+        },
+    });
+    if (response.data.status === 'OK' && response.data.results.length >= 1) {
+        return response.data.results[0].geometry.location;
     }
-    return {
-        lat: 50.45008,
-        lng: -104.6178,
-    };
+    console.error(response.data.error_message);
+    return undefined;
 }
 
-export async function reverseGeocode({ lat, lng }: LatLng): Promise<string> {
-    return 'Regina, SK';
+export async function reverseGeocode(latlng: LatLng): Promise<string> {
+    const response = await client.reverseGeocode({
+        params: {
+            latlng,
+            // we don't want the exact full address; just the general area. weather doesn't vary building to building.
+            result_type: ['locality', 'administrative_area_level_1', 'country', 'colloquial_area'],
+            key: googleGeocodingKey,
+        },
+    });
+    if (response.data.status === 'OK' && response.data.results.length >= 1) {
+        return response.data.results[0].formatted_address;
+    }
+    console.error(response.data.error_message);
+    return response.data.error_message;
 }
