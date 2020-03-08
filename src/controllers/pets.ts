a92db7171a74ca2e0d85b2c7fd7683f1cd3e8879
jonathonflorek <jonathonflorek@gmail.com>
2020-03-07 19:01:42 -0600

    some work on index controller

--- src/controllers/pets.ts
+++ src/controllers/pets.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
+import * as petShelterApi from '../integration/petShelterApi';
 
 export async function getPets(_: express.Request, res: express.Response) {
-    res.render('pets/index');
+    const pets = await petShelterApi.getAll();
+    res.render('pets/index', { pets });
 }
 
 export async function getCreatePet(_: express.Request, res: express.Response) {
--- src/integration/petShelterApi.ts
+++ src/integration/petShelterApi.ts
@@ -9,11 +9,25 @@ export interface Pet {
     };
 }
 
+const mysty: Pet = {
+    id: 0,
+    name: 'Mysty',
+    type: 'Dog',
+    breed: 'Basset',
+    location: {
+        type: 'Point',
+        coordinates: [-104.6178, 50.45008],
+    },
+}
+
 export async function getAll(): Promise<Pet[]> {
-    return [];
+    return [mysty];
 }
 
 export async function getOne(id: number): Promise<Pet | undefined> {
+    if (id === 0) {
+        return mysty;
+    }
     return undefined;
 }
 
