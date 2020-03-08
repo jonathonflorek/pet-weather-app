bd6590e5720b8a545d609e8f0f19eda83540addb
jonathonflorek <jonathonflorek@gmail.com>
2020-03-07 22:45:28 -0600

    add comment

--- src/integration/axiosConfig.ts
+++ src/integration/axiosConfig.ts
@@ -1,2 +1,3 @@
 import axios from 'axios';
+// by default, 4xx errors throw. Since we can anticipate these we should treat them as a regular result.
 axios.defaults.validateStatus = status => status >= 200 && status < 500;
